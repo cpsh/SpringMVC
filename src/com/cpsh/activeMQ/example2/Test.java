31ffc781c999412b04acfe895e068f266e66022f
shichengpeng <langzilove@126.com>
2016-01-25 18:38:27 +0800

    配置Log4j日志

--- src/com/cpsh/activeMQ/example2/Test.java
+++ src/com/cpsh/activeMQ/example2/Test.java
@@ -1,14 +1,32 @@
 package com.cpsh.activeMQ.example2;
 
 import javax.jms.JMSException;     
+
 import org.apache.activemq.ActiveMQConnection;  
+/*import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;*/
+
+import org.apache.log4j.Logger;
+
+import com.cpsh.utils.LoggerUtil;
     
 public class Test {     
+//    private static Logger logger = LoggerFactory.getLogger(Test.class);
+//    private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(Test.class);
+    
+    private static org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(Test.class);
+//    private static Logger infoLogger = Logger.getLogger("info");
     
     /**   
      * @param args   
      */    
     public static void main(String[] args) throws JMSException, Exception {     
+       LoggerUtil.info("开始测试ActiveMQ消息队列.......................");//log4j.properties生效
+       LoggerUtil.debug("开始测试ActiveMQ消息队列.......................");//log4j.properties生效
+       LoggerUtil.error("开始测试ActiveMQ消息队列.......................");//log4j.properties生效
+       LoggerUtil.warn("开始测试ActiveMQ消息队列.......................");//log4j.properties生效
+       
+       logger.info("开始测试ActiveMQ消息队列.......................");
        
      // TODO Auto-generated method stub     
         ConsumerTool consumer = new ConsumerTool();     
